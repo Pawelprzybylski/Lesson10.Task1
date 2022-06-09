f4893824b4ddf0fb5616ca10a1814b2b80a9cea8
Pawel Przybylski <pawel.przybylski1989@gmail.com>
2022-06-09 09:28:37 +0200

    Commit

---
+++ src/main/java/Content.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class Content {
+    public static void main(String[] args) throws IOException {
+        String content = Files.readString(Path.of("C:\\Users\\pawel\\Desktop\\Exercisses lesson 10\\Exercises 1.txt"));
+    }
+}
