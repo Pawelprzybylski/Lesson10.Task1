7ac926dc1b36be1c82b8c9800902e701a91eab3c
Pawel Przybylski <pawel.przybylski1989@gmail.com>
2022-06-16 13:22:01 +0200

    Commit

--- src/main/java/Content.java
+++ src/main/java/Content.java
@@ -6,4 +6,5 @@ public class Content {
     public static void main(String[] args) throws IOException {
         String content = Files.readString(Path.of("C:\\Users\\pawel\\Desktop\\Exercisses lesson 10\\Exercises 1.txt"));
     }
+
 }
